9d55562136b408432c2da07296033702881b9a0a
Anurag <anurag.khandelwal@primussoft.com>
2020-07-13 15:41:05 +0530

    Http service initialized

--- angular-playaround/src/app/app.module.ts
+++ angular-playaround/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './services/http.service';
+import { GifsComponent } from './components/gifs/gifs.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        GifsComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    providers: [HttpService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
